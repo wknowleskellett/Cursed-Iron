a469595a44a8e97bc539da57fa1f27fd57e2b373
William Knowles-Kellett <39969985+wknowleskellett@users.noreply.github.com>
2024-02-13 18:42:42 -0500

    Updated to most recent template

---
+++ src/main/java/dev/williamknowleskellett/cursed_iron/CursedIronDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.williamknowleskellett.cursed_iron;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CursedIronDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
